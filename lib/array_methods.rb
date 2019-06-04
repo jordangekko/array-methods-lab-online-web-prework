4101964849f7fc7b8b07b85181b73c25541b3dda
KRIS DAVIS <krissdavis18@mail.com>
2019-06-04 07:04:56 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,6 +2,7 @@ def using_include(array, element)
   famous_cats = ["Hello Kitty", "Simba", "Heathcliff"]
   array.include?(element)
   famous_cats.include("Garfield")
+  
 end
 
 def using_sort(array)
